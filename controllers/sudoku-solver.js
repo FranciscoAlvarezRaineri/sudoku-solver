9c83c76270616f5bd4025989bef0ede4dde9d164
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-01-27 19:49:49 -0300

    day 1

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,44 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    return puzzleString.length === 81 && /^[\.0-9]*$/.test(puzzleString);
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  splitRows(puzzleString, row) {
+    return puzzleString.match(/.{1,9}/g);
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  splitCols(puzzleString, col) {
+    let result = ["", "", "", "", "", "", "", "", ""];
+    for (let j = 0; j < 9; j++) {
+      for (let i = j; i < 81; i = i + 9) {
+        result[j] = result[j] + puzzleString[i];
+      }
+    }
+    return result;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
+  splitRegions(puzzleString, reg) {
+    let rows = this.splitRows(puzzleString);
+    let result = ["", "", "", "", "", "", "", "", ""];
+    for (let i = 0; i < 9; i++) {
+      for (let j = Math.floor(i / 3) * 3; j < Math.floor(i / 3) * 3 + 3; j++) {
+        for (let k = (i % 3) * 3; k < (i % 3) * 3 + 3; k++) {
+          console.log(i, j, k);
+          result[i] = result[i] + rows[j][k];
+        }
+      }
+    }
+    console.log(result);
+    return result;
   }
 
-  solve(puzzleString) {
-    
-  }
+  checkRowPlacement(puzzleString, row, column, value) {}
+
+  checkColPlacement(puzzleString, row, column, value) {}
+
+  checkRegionPlacement(puzzleString, row, column, value) {}
+
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,54 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
+const { suite } = require("mocha");
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const SudokuSolver = require("../controllers/sudoku-solver.js");
+const sudokus = require("../controllers/puzzle-strings").puzzlesAndSolutions;
+const sudoku1 = sudokus[0][0];
+const sudoku1Solved = sudokus[0][1];
+let solver = new SudokuSolver();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("String", () => {
+    test("Logic handles a valid puzzle string of 81 characters", (done) => {
+      assert.isTrue(solver.validate(sudoku1));
+      assert.isTrue(solver.validate(sudoku1Solved));
+      done();
+    });
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", (done) => {
+      assert.isFalse(solver.validate("hola"));
+      assert.isFalse(
+        solver.validate(
+          "11111111111111111111111111111111111111111111111111111111111111111111111111111111a"
+        )
+      );
+      done();
+    });
+    test("Logic handles a puzzle string that is not 81 characters in length", (done) => {
+      assert.isFalse(solver.validate("hola"));
+      assert.isFalse(solver.validate("111"));
+      done();
+    });
+    suite("Placement", () => {
+      test("Logic handles a valid row placement", (done) => {
+        done();
+      });
+      test("Logic handles an invalid row placement", (done) => {
+        done();
+      });
+      test("Logic handles a valid column placement", (done) => {
+        done();
+      });
+      test("Logic handles an invalid column placement", (done) => {
+        done();
+      });
+      test("Logic handles a valid region (3x3 grid) placement", (done) => {
+        done();
+      });
+      // Logic handles an invalid region (3x3 grid) placement
+    });
+  });
+  // Valid puzzle strings pass the solver
+  // Invalid puzzle strings fail the solver
+  // Solver returns the expected solution for an incomplete puzzle
 });
